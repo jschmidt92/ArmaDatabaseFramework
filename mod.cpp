d07b3ba5e0eed553f9584a0fadf287de07195435
Jacob Schmidt <innovativestudios@outlook.com>
2024-07-28 16:59:47 -0500

    Added placeholder icons, adf_version.json and Mod.cpp

---
+++ mod.cpp
@@ -0,0 +1,21 @@
+dir = "@ArmaDatabaseFramework";
+name = "ArmaDatabaseFramework";
+
+author = "J.Schmidt and NikolaiF90";
+
+picture         = "title_co.paa";                                       // Picture displayed from the expansions menu. Optimal size is 2048x1024
+hideName        = "false";                                              // Hide the extension name
+hidePicture     = "false";                                              // Hide the extension menu
+
+logoSmall       = "icon_64_ca.paa";                                  // Display next to the item added by the mod 64x64
+logo            = "icon_128_ca.paa";                                 // Logo displayed in the main menu 128x128
+logoOver        = "icon_128_highlight_ca.paa";                       // When the mouse is over, in the main menu 128x128
+tooltip         = "ArmaDatabaseFramework";
+tooltipOwned    = "ArmaDatabaseFramework";                                // Tool tip displayed when the mouse is left over, in the main menu
+overview        = "Arma 3 ArmaDatabaseFramework Modification";            // Overview text, displayed from the extension menu
+description     = "It's unclear where this will show";                  // Probably in context with action
+action          = "https://github.com/jschmidt92/ArmaDatabaseFramework";  // Website URL, that can accessed from the expansions menu 
+actionName      = "Website";                                // label of button/tooltip in extension menu
+
+// Color used for DLC stripes and backgrounds (RGBA)
+dlcColor[] = {0.45, 0.47, 0.41, 1};
